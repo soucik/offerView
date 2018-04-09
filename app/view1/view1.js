7e183c1cd48b9f8f57da01d73b3af5434019850f
Pavol Šoučík <soucik@gmail.com>
2018-04-09 10:13:49 +0200

    Added: loaded offer parsing, exploring, styling.  libs: bootstrap, momentjs

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,65 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .service('commonService', function ($http) {
 
-.controller('View1Ctrl', [function() {
+    // Service to get file offer.json
+    this.getOffer = function () {
+      return $http.get("offer.json");
+    };
 
-}]);
+    // Makes Array from object
+    this.objToArray = function (data) {
+      var arr = [],
+        keys = Object.keys(data);
+      for (var i = 0, n = keys.length; i < n; i++) {
+        var key = keys[i];
+        arr[i] = data[key];
+      }
+      return arr;
+    };
+  })
+
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+
+  .controller('View1Ctrl', ['commonService', '$scope', '$http', 'moment', function (commonService, $scope, $http, moment) {
+
+    // Offer Parser, returns parsed offer
+    $scope.parseOffer = function (offerToParse) {
+      var eventChanceTypesList = offerToParse.EventChanceTypes;
+      $scope.oddsList = commonService.objToArray(offerToParse.Odds); // assign Array list of odds to view scope
+      var labelsList = commonService.objToArray(offerToParse.Labels); // assign Array list of labels
+      var leaguesList = labelsList.filter(league => { return league.Typ == 'LC' }); // list of leags
+      var regionsList = labelsList.filter(region => { return region.Typ == 'RE' }); // list of regions
+      var sportList = labelsList.filter(sport => { return sport.Typ == 'SP' }); // list of sports
+
+      // Maps events within leagues
+      var leaguesEvents = leaguesList.map(league => {
+        return {
+          LeagueName: league.Name,
+          Events: eventChanceTypesList.filter(event => event.LeagueCupID == league.ID)
+        }
+      });
+
+      // Assigns spor name based on event argument
+      var assignSportName = function (event) {
+        return sportList.filter(sport => sport.ID == event.SportID).map(sport => sport.Name).toString()
+      }
+
+      // Makes leag Events with sports name
+      Object.keys(leaguesEvents).forEach(function (key, index) {
+        leaguesEvents[key].SportName = assignSportName(leaguesEvents[key].Events[0]);
+      });
+      return leaguesEvents;
+    }
+
+    commonService.getOffer()
+      .success(function (offer) {
+        $scope.Leagues = $scope.parseOffer(offer); // Parse respoded offer and assign it to view scope
+      });
+  }]);
